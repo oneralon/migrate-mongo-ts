c4b42af5d8afc4339d06a4915ab3d37d02d25e23
oneralon <oneralon@gmail.com>
2021-07-05 12:19:23 +0300

    Remove default down function

--- samples/migration.ts
+++ samples/migration.ts
@@ -6,9 +6,3 @@ export async function up(db: Db) {
   // Example:
   // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: true}});
 }
-
-export async function down(db: Db) {
-  // TODO write the statements to rollback your migration (if possible)
-  // Example:
-  // await db.collection('albums').updateOne({artist: 'The Beatles'}, {$set: {blacklisted: false}});
-}
